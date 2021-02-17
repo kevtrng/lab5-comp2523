4fdcc28a73969258a9189bc5b4e059807e180bfe
Kev Truong <kevtrng00@gmail.com>
2021-02-17 02:14:41 -0800

    ISortable and NumberGroup Updated

--- src/ISortable.ts
+++ src/ISortable.ts
@@ -1,6 +1,6 @@
 export interface ISortable {
   length: number;
   compare(leftPos: number, rightPos: number): boolean;
-  swap(left: number, right: number)
+  swap(leftPos: number, rightPos: number);
 }
 
--- src/NumberGroup.ts
+++ src/NumberGroup.ts
@@ -1,3 +1,27 @@
-export class NumberGroup {
+import { ISortable} from './ISortable';
+
+export class NumberGroup implements ISortable{
+    constructor (data: number[]){
+        this.data = data;
+    }
+
     data: number[];
+
+    get length(): number {
+        return this.data.length
+    }
+    compare(leftPos: number, rightPos: number): boolean {
+        if (this.data[leftPos] > this.data[rightPos]) {
+            return true;
+        } else {
+            return false; 
+        }
+    }
+    swap(leftPos: number, rightPos: number): void{
+        let left = this.data[leftPos];
+        this.data[leftPos] = this.data[rightPos];
+        this.data[rightPos] = left
+    }
+
+
   }
